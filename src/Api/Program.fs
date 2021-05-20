cc17ad5bf849ebdde01d3c0cf6203f4ed0609f9a
Matthew James <mjames4@woolworths.com.au>
2021-05-20 15:30:05 +1000

    Provide simple way to create and modify product

--- src/Api/Program.fs
+++ src/Api/Program.fs
@@ -11,6 +11,8 @@ open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 
+open Domain
+
 module Program =
     let exitCode = 0
 
@@ -23,5 +25,8 @@ module Program =
     [<EntryPoint>]
     let main args =
         CreateHostBuilder(args).Build().Run()
+
+        let a = Product.create "PD1" "Product Name" 99.95m
+        let b = Product.setName "Blah" a
 
         exitCode
---
+++ src/Domain/Product.fs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+
+type Product = private { id: string; name: string; price: decimal }
+
+module Product =
+    let create id name price =
+        { id = id; name = name; price = price }
+
+    let setName name product =
+        { product with name = name }
+
+    let setPrice price product =
+        { product with price = price }
