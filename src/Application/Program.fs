33dbb2d6fcaadaecbd08a09c4fec581b2df3b546
Matthew James <mjames4@woolworths.com.au>
2021-05-21 14:04:01 +1000

    Use simple console application to run

---
+++ src/Application/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open Domain
+
+[<EntryPoint>]
+let main _ =
+    let p =
+        Product.create "PD1" "Product Name" 99.95m
+        |> Result.bind (Product.setName "Blah")
+        |> Result.bind (Product.setPrice 49.95m)
+
+    match p with
+    | Ok product -> printfn "We have a product %s" (Product.getName product)
+    | Error e -> match e with
+                    | CreationError creationErrors -> List.iter (fun (error: string) -> Console.WriteLine(error)) creationErrors
+                    | SetNameError error -> Console.WriteLine(error)
+                    | SetPriceError error -> Console.WriteLine(error)
+
+    0
